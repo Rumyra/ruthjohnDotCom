d7686ea8de35a82bd2e99daec9ada88fc17397a6
Ruth John <rumyra@gmail.com>
2017-06-23 22:17:13 +0100

    start of the grid js

--- public/whitepaper/js/app.js
+++ public/whitepaper/js/app.js
@@ -18,3 +18,50 @@ PageController.prototype.changePage = function() {
   console.log(this.allPages);
   this.allPages.forEach(showHidePage);
 }
+
+// grid stuff
+// higher number, less of a space hog
+var spaceHog = 1.6;
+
+// get container size
+const container = document.querySelector('.layout');
+const contWidth = container.getBoundingClientRect().width;
+
+// get item size
+const items = document.querySelectorAll('.layout li');
+const itemWidth = items[0].getBoundingClientRect().width;
+
+// divide container by item
+// half that is rough row item count
+const roughColCount = Math.floor( (contWidth/itemWidth)/spaceHog );
+
+// get total items
+const totalItems = items.length;
+
+// divide by rough row item count gives row count
+const roughRowCount = Math.floor(totalItems/roughColCount);
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+// loop over each row
+for (let i=0; i<roughRowCount; i++) {
+  
+  // while counter is less than itemcount
+  var itemCounter = 0;
+  while (itemCounter<totalItems) {
+    // randomise row item count
+    var colCount = randomColCount+(getRandomIntInclusive(-3,3));
+    console.log(colCount);
+    
+    itemCounter++;
+  }
+  
+  
+}
+
+// randomise row offset
+// for rowcount place items
